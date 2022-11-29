e141a525182724669cc7b31cbfc59744451b9d61
smlemons <lena.monsch@myquix.de>
2022-11-29 15:30:57 +0100

    Create Umfrage.Ernaehrung.R

---
+++ Umfrage.Ernaehrung.R
@@ -0,0 +1,25 @@
+## Etech Datensatz Ernaehrung
+
+load("~/Umfrage.RData")
+
+head(Umfrage)
+
+## Umlaute
+## Warum X.3?
+## Klammer bei ehemaligen
+
+# tabelle zu ernaehrungsform, barplot
+# Umfeld korrelation ernährungstyp -> Tabelle mit 2 Variablen
+# zsmhang ernährungstyp und auf was achtest du (X3)(bivariat), tabelle für x3
+# hist für finanzielles, streudiagramm !!
+# mosaikplot zu frage 6, barplot mit stufen
+# barplot mit Stufen für Mensa Ranking
+# angebot der Mensa barplot/Tabelle nach ernÄhrungstyp(Mittelwert)
+# boxplot/mean() nach Ernährungstyp
+# geschlecht filtern nach Ernährungstyp
+# Wohnsituation im Vgl mit Tage + Finanzielles  abhängig von Ergebnissen vorher
+# studiengänge gruppieren und nach ernährungstyp
+# abschluss und geld
+
+
+
